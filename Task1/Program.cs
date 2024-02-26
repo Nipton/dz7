5188809f0f04a1b04d1154e63fdf1ac8438a2650
Nipton <Nipton@NIPTON>
2024-02-27 00:18:35 +0400

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void PrintNaturalNumbers(int m, int n)
+        {
+            if (m == n)
+            {
+                Console.Write(m);
+                return;
+            }
+            Console.Write(m + ", ");
+            PrintNaturalNumbers(m + 1, n);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (m <= n)
+                PrintNaturalNumbers(m, n);
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void PrintReversedArray(int[] arr, int i)
+        {
+            if (i < 0)
+                return;
+            Console.Write(arr[i] + " ");
+            PrintReversedArray(arr, i - 1);
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(100);
+            }
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            PrintReversedArray(arr, arr.Length - 1);
+        }
+    }
+}
